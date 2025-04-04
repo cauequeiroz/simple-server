f4b7228a3af41d7d75b21f4944f4d63dd7020351
Caue Queiroz <cauenqueiroz@gmail.com>
2025-04-04 18:02:02 -0300

    add websocket connection

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const server = require('http').createServer();
+const WebSocketServer = require('ws').Server;
+const websocket = new WebSocketServer({ server: server });
 
 const app = express();
 
+// Basic Response
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
+});
+
+// WebSocket Connection
+websocket.on('connection', socket => {
+    if (socket.readyState === socket.OPEN) {
+        socket.send('Welcome to my WebSocket Server!');
+    }
+
+    websocket.clients.forEach(client => client.send(`Number of Visitors: ${websocket.clients.size}`))
 });
 
 server.on('request', app);
